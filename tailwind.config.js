78ab3f8c1abb3374e60cf3dcd90958e579e404c6
Luuk Janssens <git@luukjanssens.nl>
2024-08-15 20:28:41 +0200

    Initialize light mode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: ['class'],
+module.exports = {
+  lightMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
